95a4e2547aa98dc4c92599edc5c49a5f79897d68
unknown <Daniel.Maydana@fundacion-jala.org>
2019-11-22 00:00:57 -0400

    BE: ListIterator

---
+++ BE/Algorithms/Algorithms/Iterators/ListIterator.cs
@@ -0,0 +1,49 @@
+﻿using Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Iterators
+{
+    public class ListIterator : IIterator
+    {
+        private IList list;
+        public uint index;
+
+        public ListIterator(IList list)
+        {
+            this.list = list;
+            this.index = 0;
+        }
+
+        public object Current()
+        {
+            return this.list.Get(index);
+        }
+
+        public void First()
+        {
+            this.index = 0;
+        }
+
+        public bool IsDone()
+        {
+            return this.index == 0 || this.index == this.list.Size() - 1U;
+        }
+
+        public void Last()
+        {
+            this.index = this.list.Size() - 1U;
+        }
+
+        public void Next()
+        {
+            this.index++;
+        }
+
+        public void Previous()
+        {
+            this.index--;
+        }
+    }
+}
---
+++ BE/Algorithms/Algorithms/UnitTestProject/ListIteratorTesting.cs
@@ -0,0 +1,119 @@
+﻿using Algorithms;
+using Iterators;
+using Lists;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using UnitTestProject.Mocks;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class ListIteratorTesting
+    {
+        [TestMethod]
+        public void Current_ReturnsFirstPosition_WhenInstantiatedWithAFakeLinkedLlist()
+        {
+            IIterator myLLIterator = new ListIterator(new FakeLinkedList());
+
+            var actual = myLLIterator.Current();
+
+            var expected = 111;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Current_ReturnsFirstPosition_WhenFirstCalled()
+        {
+            IIterator myLLIterator = new ListIterator(new FakeLinkedList());
+
+            myLLIterator.First();
+
+            var actual = myLLIterator.Current();
+
+            var expected = 111;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Current_ReturnsLastPosition_WhenLastCalled()
+        {
+            IIterator myLLIterator = new ListIterator(new FakeLinkedList());
+
+            myLLIterator.Last();
+
+            var actual = myLLIterator.Current();
+
+            var expected = 555;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Current_ReturnsSecondPosition_WhenNextCalledOnce()
+        {
+            IIterator myLLIterator = new ListIterator(new FakeLinkedList());
+
+            myLLIterator.Next();
+
+            var actual = myLLIterator.Current();
+
+            var expected = 222;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Current_ReturnsPenultimatePosition_WhenPreviousCalledOnce()
+        {
+            IIterator myLLIterator = new ListIterator(new FakeLinkedList());
+
+            myLLIterator.Last();
+
+            myLLIterator.Previous();
+
+            var actual = myLLIterator.Current();
+
+            var expected = 444;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IsDone_ReturnsTrue_WhenLastCalled()
+        {
+            IIterator myLLIterator = new ListIterator(new FakeLinkedList());
+
+            myLLIterator.Last();
+
+            var actual = myLLIterator.IsDone();
+
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void IsDone_ReturnsTrue_WhenFirstCalled()
+        {
+            IIterator myLLIterator = new ListIterator(new FakeLinkedList());
+
+            myLLIterator.First();
+
+            var actual = myLLIterator.IsDone();
+
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void IsDone_ReturnsFalse_WhenNextCalledOnce()
+        {
+            IIterator myLLIterator = new ListIterator(new FakeLinkedList());
+
+            myLLIterator.Next();
+
+            var actual = myLLIterator.IsDone();
+
+            Assert.IsFalse(actual);
+        }
+    }
+}
